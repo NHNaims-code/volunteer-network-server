651c25465798a8ade0e42acca7f6ab1a3807d7dd
NHNaims-code <mdnimurhasan99@gmail.com>
2020-10-05 23:30:24 +0600

    finish

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const port = 5000
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const pass = '9XUV1bmUUK7bYkcj'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://vollunteer-network:${pass}@cluster0.sjmet.mongodb.net/vollunteer-network>?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const eventCollection = client.db("vollunteer-network").collection("events");
+  const registerCollection = client.db("vollunteer-network").collection("registers");
+  
+    app.post('/addevent', (req, res) => {
+        const event = req.body;
+        eventCollection.insertOne(event)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.get('/events', (req, res) => {
+        eventCollection.find({}).toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+
+    app.post('/register', (req, res) => {
+        const registerData = req.body;
+        registerCollection.insertOne(registerData)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.get('/user/:email', (req, res) => {
+        registerCollection.find({email: req.params.email})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+        
+    })
+    app.get('/users', (req, res) => {
+        registerCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+        
+    })
+
+    app.get('/profileEvent/:title', (req, res) => {
+        eventCollection.find({title: req.params.title})
+        .toArray((err, documents)=>{
+            res.send(documents)
+        })
+    })
+
+});
+
+
+app.listen(port);
