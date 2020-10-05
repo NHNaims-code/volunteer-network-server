7f3ffbee587bf2dc2d1c9e292a042ebe75e79fbb
NHNaims-code <mdnimurhasan99@gmail.com>
2020-10-05 23:38:35 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = 5000
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const pass = '9XUV1bmUUK7bYkcj'
+
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://vollunteer-network:${pass}@cluster0.sjmet.mongodb.net/vollunteer-network>?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjmet.mongodb.net/vollunteer-network>?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
   const eventCollection = client.db("vollunteer-network").collection("events");
