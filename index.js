13c51244e304fb75097cf9c0c97d2ed09ed9e6a1
NHNaims-code <mdnimurhasan99@gmail.com>
2020-10-06 16:46:04 +0600

    lot of bug fixed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const port = 5000
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+const port = 5000
 const cors = require('cors');
-const pass = '9XUV1bmUUK7bYkcj'
+
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,12 +16,12 @@ app.get('/', (req, res) => {
 })
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://vollunteer-network:${pass}@cluster0.sjmet.mongodb.net/vollunteer-network>?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjmet.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
   const eventCollection = client.db("vollunteer-network").collection("events");
   const registerCollection = client.db("vollunteer-network").collection("registers");
+  eventCollection.createIndex( { title: "text" } )
   
     app.post('/addevent', (req, res) => {
         const event = req.body;
@@ -62,6 +65,27 @@ client.connect(err => {
         eventCollection.find({title: req.params.title})
         .toArray((err, documents)=>{
             res.send(documents)
+        })
+    })
+
+    app.delete('/cancle/:id',(req, res) => {
+        console.log(req.params.id);
+        registerCollection.deleteOne({_id:ObjectId(req.params.id)})
+        .then(result => {
+            res.send(result.deletedCount > 0);
+        })
+    })
+
+    app.delete('/delete/:email', (req, res) => {
+        registerCollection.deleteMany({email:req.params.email})
+        .then(result => {
+            res.send(result.deletedCount > 0)
+        })
+    })
+
+    app.get('/search', (req, res) => {
+        eventCollection.find({ $text: { $search: req.query.search } }).toArray((err, documents) => {
+            res.send(documents);
         })
     })
 
